2ae5670abbcecb620e8850c6065988992789a088
Rodrigo Petry <rodrigo.petry18@gmail.com>
2018-11-22 20:40:06 -0200

    Merge Conflict Fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Routes } from '@angular/router'
 
 import { HomeComponent } from './home/home.component'
@@ -10,13 +9,3 @@ export const ROUTES: Routes = [
   { path: "about", component: AboutComponent },
   { path: "restaurants", component: RestaurantsComponent }
 ]
-=======
-import { Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component"
-import { AboutComponent } from "./about/about.component"
-
-export const ROUTES: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "about", component: AboutComponent }
-];
->>>>>>> 76e9e06068ef129eafb55f31deee1cce44ae7a71
